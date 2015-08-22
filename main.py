a0c22c86f4acc0f147c620c02753ac3f566dc6f8
liujingyu <jingyu52@gmail.com>
2015-08-22 14:09:05 +0800

    试用版

--- main.py
+++ main.py
@@ -43,14 +43,18 @@ def main():
         sys.exit()
     else:
         with open(config.CHANNEL_FILE, 'r') as f:
-            channels = [ line.strip() for line in f.readlines()]
+            channels = [ line.strip() for line in f.readlines() if line.strip()]
 
         if len(channels) == 0:
             print '渠道文件不能为空, 请在', config.CHANNEL_FILE , '文件里添加渠道名字'
             print '='*60
             sys.exit()
 
+    print '当前设置的version值', config.VERSION
+    version = raw_input(">>>input version eg:3.3.1 or ''\n>>>version = ") or config.VERSION
+
     print '正在复制原始包'
+    print
 
     t = time.strftime('%Y-%m-%d-%H_%M_%S')
     if os.mkdir(config.APK_DIR+t) is False:
@@ -60,9 +64,13 @@ def main():
 
     #单线程复制&添加渠道
     try:
+
+        start = time.time()
         for channel in channels:
 
-            new_channel_name = '_'.join([config.APK_PREFIX, channel, config.VERSION]) + config.APK_EXT
+            print '='*20, channel, '='*20
+
+            new_channel_name = '_'.join([config.APK_PREFIX, channel, version]) + config.APK_EXT
 
             print 'copy', config.ORIGIN_APK_NAME, ' -> ', new_channel_name
             shutil.copy(config.ORIGIN_APK_NAME, new_channel_name)
@@ -72,10 +80,17 @@ def main():
                 print '='*60
                 sys.exit()
             else:
+                print 'write channel', channel, 'is successful'
 
                 print 'move', new_channel_name, ' -> ', config.APK_DIR + t
                 shutil.move(new_channel_name, config.APK_DIR + t)
 
+            print '='*20, channel, '='*20
+            print
+
+        end = time.time()
+        print '复制了', len(channels), '个包'
+        print '复制耗时', end-start, 's'
     except Exception as e:
         print e
     else:
